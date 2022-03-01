fcf751e90b13dc92f6add33118c587a0c400c8d3
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-02-28 22:32:30 -0600

    added another test

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,6 +1,9 @@
-const checkIfEqual = require('../lib/random.js');
+const randomNumber = require('../lib/random.js');
 // use this relative path when going back a folder into root and into another folder
 
-test('checks if 10 is equal to 10', () => {
-   expect(checkIfEqual(10, 10)).toBe(true);
+test('gets random number between 1 and 10', () => {
+
+   expect(randomNumber()).toBeGreaterThanOrEqual(1);
+   expect(randomNumber()).toBeLessThanOrEqual(10);
+   
 });
